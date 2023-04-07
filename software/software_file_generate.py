9e004e84eb730f5566fe70984f49ce8d042d11cf
Kai <gkai70264@gmail.com>
2023-04-07 19:29:45 -0400

    add new binary support and fix this

--- software/software_file_generate.py
+++ software/software_file_generate.py
@@ -12,9 +12,15 @@ for i in os.listdir("."):
     if pathlib.Path(i).is_dir():
         packagetoml = open(i + "/package.toml")
         tomlpac = toml.load(packagetoml)
-        for ic in tomlpac["main"]["files"]:
+        for ic in tomlpac["main"]["scripts"]:
             name = pathlib.Path(ic).stem
             final_dict[name] = pathlib.Path(i).stem
+        try:
+            for ic in tomlpac["main"]["binaries"]:
+                name = pathlib.Path(ic).stem
+                final_dict[name] = pathlib.Path(i).stem
+        except KeyError:
+            pass
         packagetoml.close()
 
 di = json.dumps(final_dict)
