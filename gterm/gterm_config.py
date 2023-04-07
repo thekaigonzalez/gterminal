085b65e3a63d3b6228f691a4e7589e6575fe9095
Kai <gkai70264@gmail.com>
2023-04-07 19:36:32 -0400

    moving away from account in same config

--- gterm/gterm_config.py
+++ gterm/gterm_config.py
@@ -6,7 +6,22 @@ def get_local_config():
     c.read("system/system.cfg")
 
     return c
+def get_account():
+
+    c = configparser.ConfigParser()
+    c.read("system/credentials.cfg")
+
+    return c["account"]
 
 def get_version():
     return get_local_config()["System"]["version"]
 
+class Account:
+    def __init__(self) -> None:
+        self.profile = get_account()
+        pass
+    def get_name(self):
+        return self.profile["name"]
+    def has_bios_access(self):
+        return self.profile["bios"]
+    
