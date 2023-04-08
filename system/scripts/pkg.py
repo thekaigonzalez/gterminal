d53e3bf1f314d9c271f739432f111002fb643d04
Kai <gkai70264@gmail.com>
2023-04-08 12:04:22 -0400

    new library installation features

--- system/scripts/pkg.py
+++ system/scripts/pkg.py
@@ -51,6 +51,35 @@ def pkg_install(prog: str):
     print("---------")
     time.sleep(1)
     print("package installed :D ! enjoy.")
+
+def pkg_install_lib(libn: str):
+    libn = libn.strip()
+    if (len(libn) == 0):
+        print("need library to install ?")
+        return
+    
+    print("pkg: installing {} . . .".format(libn))
+    url = "https://raw.githubusercontent.com/thekaigonzalez/gterminal/master/software/" + libn
+    pkg_file = requests.get(url + "/library.toml")
+    
+    if (pkg_file.status_code != 200):
+        print("error: package was not found in repo")
+        return -1
+    loader = toml.loads(pkg_file.text)
+
+    # https://raw.githubusercontent.com/thekaigonzalez/hello/master/hello.py
+    time.sleep(2)
+
+    print("pkg: name: " + loader["main"]["name"])
+    print("pkg: version: " + loader["main"]["version"])
+
+    for file in loader["main"]["files"]:
+        print("lib: " + file + " !")
+        fileinstall = requests.get(url + file)
+        with open("system/lib/" + os.path.basename(file), "w") as f:
+            f.write(fileinstall.text)
+        time.sleep(random.uniform(0.5, 0.01))
+
 def onExecute(args: list):
     if (len(args) == 1):
         print(
@@ -65,3 +94,6 @@ and pkg/dup from Kux
         if (get_arg(args, 1) == "get"):
             package = get_arg(args, 2)
             pkg_install(package)
+        elif get_arg(args, 1) == "download":
+            library = get_arg(args, 2)
+            pkg_install_lib(library)
