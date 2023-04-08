431e6835f65740f3dacec394fc533e5f9244a0d8
Kai <gkai70264@gmail.com>
2023-04-08 12:01:46 -0400

    fix if packagetoml exists

--- software/software_file_generate.py
+++ software/software_file_generate.py
@@ -3,25 +3,25 @@ import pathlib
 import toml
 import json
 
-
 # generate a file with connection from commands to packages in a json file
 
 final_dict = {}
 
 for i in os.listdir("."):
     if pathlib.Path(i).is_dir():
-        packagetoml = open(i + "/package.toml")
-        tomlpac = toml.load(packagetoml)
-        for ic in tomlpac["main"]["scripts"]:
-            name = pathlib.Path(ic).stem
-            final_dict[name] = pathlib.Path(i).stem
-        try:
-            for ic in tomlpac["main"]["binaries"]:
+        if (pathlib.Path(i + "/package.toml").exists()):
+            packagetoml = open(i + "/package.toml")
+            tomlpac = toml.load(packagetoml)
+            for ic in tomlpac["main"]["scripts"]:
                 name = pathlib.Path(ic).stem
                 final_dict[name] = pathlib.Path(i).stem
-        except KeyError:
-            pass
-        packagetoml.close()
+            try:
+                for ic in tomlpac["main"]["binaries"]:
+                    name = pathlib.Path(ic).stem
+                    final_dict[name] = pathlib.Path(i).stem
+            except KeyError:
+                pass
+            packagetoml.close()
 
 di = json.dumps(final_dict)
 
