86f579a9c4cbe5389e720f1930e3e807e4537529
Kai <gkai70264@gmail.com>
2023-04-08 12:11:36 -0400

    add an easy way to import all sys libraries

---
+++ gterm/gterm_all.py
@@ -0,0 +1,9 @@
+import gterm.gterm_argmg as GTArg
+import gterm.gterm_colorama as GTColors
+import gterm.gterm_config as GTConfig
+import gterm.gterm_directory as GTDirs
+import gterm.gterm_filesystem as GTFS
+import gterm.gterm_helpers as GTHelpers
+import gterm.gterm_libmgr as GTLibs
+
+import gterm.shell as GTShell
