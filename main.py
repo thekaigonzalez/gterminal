81634709938f590ca148e316df5de8f9d349a41e
Kai <gkai70264@gmail.com>
2023-04-09 21:02:51 -0400

    gterm NEW ACCOUNT SYSTEm

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import sys
 import readline
 
 import gterm.shell as gsh
+from gtacc.Info import check_and_ask
+from gtacc.Send import stringify
 
 # this is where it begins
 
@@ -11,4 +13,9 @@ import pathlib
 if (pathlib.Path("system/bin").exists == False):
     os.mkdir("system/bin")
 
-gsh.start_shell()
+response = check_and_ask()
+
+if (stringify(response) == 'F_OK'):
+    gsh.start_shell()
+else:
+    print("login to gsh failed.")
